62fdcecae1e830970797506685fe158c78a29781
zh10034 <zh10034@LAPTOP-KH5314B2.localdomain>
2023-10-26 19:24:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
-}
+function addingEventListener() { 
+    const input = document.getElementById('button');
+input.addEventListener('click',  function() {
+    alert('HeHe that tickles ;)')
+})
+};
